e51a8cfe6f91f5b38c7ceef5ca9c2e7b469b5b59
Carlos Polo <Zolrak22@users.noreply.github.com>
2024-04-30 14:21:47 -0400

    Stack and Heap Practice

--- Tutorial1/Source.cpp
+++ Tutorial1/Source.cpp
@@ -6,116 +6,42 @@
 #include <string>
 using std::cout; using std::cin; using std::endl; using std::string;   //std namespace usage
 
-class Creature
+struct Character
 {
-public:
-	Creature();
-
-	void SetName(string name);
-	string GetName();
-
-	float GetHealth();
-	
-	void TakeDamage(float damage);
-
-	void DisplayName(string name);
-	void DisplayHealth(float health);
-	void DisplayLevel(int Level);
-
-private:
+	Character();
+	void PrintHealth();
 	string Name;
 	float Health;
-
-protected:
-	int Level;
-
-};
-
-class Goblin : public Creature
-{
-public:
-	Goblin();
-
-	int GetLevel();
 };
 
 int main()
 {
-	Goblin Gobby;
+	for (int i = 0; i < 10; i++)
+	{
+		Character* PtrToChar = new Character(); // Stored in the Heap
 
-	Gobby.DisplayName(Gobby.GetName());
-	Gobby.DisplayHealth(Gobby.GetHealth());
-	Gobby.DisplayLevel(Gobby.GetLevel());
+		cout << PtrToChar->Name << endl;
+
+		PtrToChar->PrintHealth();
+
+		delete PtrToChar;
+	}
+
+
 
 	system("pause");
 	return 0;
 }
 
-Creature::Creature()
+Character::Character()
 {
+	Name = "Default Name";
 	Health = 100.0f;
-	cout << "A creature has been created! " << endl;
 }
 
-void Creature::SetName(string name)
+void Character::PrintHealth()
 {
-	Name = name;
-}
-
-string Creature::GetName()
-{
-	return Name;
-}
-
-
-float Creature::GetHealth()
-{
-	return Health;
-}
-
-void Creature::TakeDamage(float damage)
-{
-	float TotalHealth;
-	TotalHealth = Health - damage;
-
-	if (TotalHealth <= 0.0f)
-	{
-		cout << GetName() << " has died!" << endl;
-	}
-
-	else
-	{
-		Health -= damage;
-	}
-
-	cout << "Health: " << GetHealth() << endl;
-}
-
-Goblin::Goblin()
-{
-	Level = 1;
-
-	SetName("Gobby");
-}
-
-int Goblin::GetLevel()
-{
-	return Level;
-}
-
-void Creature::DisplayName(string name)
-{
-	cout << "Name: " << name << endl;
-}
-
-void Creature::DisplayHealth(float health)
-{
-	cout << "Health: " << health << endl;
-}
-
-void Creature::DisplayLevel(int level)
-{
-	cout << "Level: " << level << endl;
+	cout << "Health = " << Health << endl;
 }
 
 //// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
